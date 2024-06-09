0ba0f7c85b0a2982616758ef2283f735140b70c3
Junior flores <fmjg21@gmail.com>
2024-06-09 17:35:00 -0500

    add tif_boundaries

--- data_process/process_geojson.sh
+++ data_process/process_geojson.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # create folder
 outputDir=csv_file
+export AWS_PROFILE=personal
 
 mkdir -p $outputDir
 
@@ -10,27 +11,37 @@ countries=("vietnam:VNM"
             "nepal:NPL"
             "afghanistan:AFG"
             "pakistan:PAK" )
+
 for i in "${countries[@]}" ;
  do
    KEY="${i%%:*}"
    VALUE="${i##*:}"
-#
-#   python src/clean_data.py \
-#      --geojson_inp=data/osm/geojson/${KEY}_poi_education.geojson \
-#      --geojson_geometry=data/geoboundaries/${VALUE}__ADM0.geojson \
-#      --csv_out=${outputDir}/${KEY}_education.csv.gz
-#
-#    python src/clean_data.py \
-#        --geojson_inp=data/osm/geojson/${KEY}_healthcare.geojson \
-#        --geojson_geometry=data/geoboundaries/${VALUE}__ADM0.geojson \
-#        --csv_out=${outputDir}/${KEY}_healthcare.csv.gz
-#
-#    python src/clean_data.py \
-#        --geojson_inp=data/osm/geojson/${KEY}_transport.geojson \
-#        --geojson_geometry=data/geoboundaries/${VALUE}__ADM0.geojson \
-#        --csv_out=${outputDir}/${KEY}_transport.csv.gz
-#
-#  echo "---- done  ${i} ------"
+
+   python src/clean_data.py \
+      --geojson_inp=data/osm/geojson/${KEY}_poi_education.geojson \
+      --geojson_geometry=data/geoboundaries/${VALUE}__ADM0.geojson \
+      --csv_out=${outputDir}/${KEY}_education.csv.gz
+
+    python src/clean_data.py \
+        --geojson_inp=data/osm/geojson/${KEY}_healthcare.geojson \
+        --geojson_geometry=data/geoboundaries/${VALUE}__ADM0.geojson \
+        --csv_out=${outputDir}/${KEY}_healthcare.csv.gz
+
+    python src/clean_data.py \
+        --geojson_inp=data/osm/geojson/${KEY}_transport.geojson \
+        --geojson_geometry=data/geoboundaries/${VALUE}__ADM0.geojson \
+        --csv_out=${outputDir}/${KEY}_transport.csv.gz
+
+    # prepare cog
+    rio cogeo create data/tif_boundaries/${VALUE}.tif \
+        data/tif_boundaries/${VALUE}_cog.tif \
+        --blocksize 512 \
+        --co JPEG_QUALITY=100 \
+        --nodata -99999 \
+        -w
+
+  echo "---- done  ${i} ------"
 done
-# mv
-cp -r $outputDir ../app/public/assets/
+## mv
+#cp -r $outputDir ../app/public/assets/
+
