cd49fd176e75cfbdfdd955419dc8ecaaa3300d5e
Junior flores <fmjg21@gmail.com>
2024-06-11 14:56:41 -0500

    update csv and bash

--- data_process/process_geojson.sh
+++ data_process/process_geojson.sh
@@ -10,7 +10,8 @@ countries=("vietnam:VNM"
             "myanmar:MMR"
             "nepal:NPL"
             "afghanistan:AFG"
-            "pakistan:PAK" )
+            "pakistan:PAK"
+              )
 
 for i in "${countries[@]}" ;
  do
@@ -32,16 +33,45 @@ for i in "${countries[@]}" ;
         --geojson_geometry=data/geoboundaries/${VALUE}__ADM0.geojson \
         --csv_out=${outputDir}/${KEY}_transport.csv.gz
 
-    # prepare cog
-    rio cogeo create data/tif_boundaries/${VALUE}.tif \
-        data/tif_boundaries/${VALUE}_cog.tif \
+#    # prepare cog
+#    rio cogeo create data/tif_boundaries/${VALUE}.tif \
+#        data/tif_boundaries/${VALUE}_cog.tif \
+#        --blocksize 512 \
+#        --co JPEG_QUALITY=100 \
+#        --nodata -99999 \
+#        -w
+
+    python src/points2raster.py \
+          --geojson_path=data/osm/geojson/${KEY}_poi_education.geojson \
+          --boundary_path=data/geoboundaries/${VALUE}__ADM0.geojson \
+          --tif_output=data/tif_boundaries/${VALUE}_education.tif
+
+    python src/points2raster.py \
+        --geojson_path=data/osm/geojson/${KEY}_healthcare.geojson \
+        --boundary_path=data/geoboundaries/${VALUE}__ADM0.geojson \
+        --tif_output=data/tif_boundaries/${VALUE}_healthcare.tif
+
+    python src/points2raster.py \
+      --geojson_path=data/osm/geojson/${KEY}_transport.geojson \
+      --boundary_path=data/geoboundaries/${VALUE}__ADM0.geojson \
+      --tif_output=data/tif_boundaries/${VALUE}_transport.tif
+
+
+      python src/index_services.py \
+        --population_path=data/tif_boundaries/${VALUE}.tif \
+        --education_path=data/tif_boundaries/${VALUE}_education.tif \
+        --healthcare_path=data/tif_boundaries/${VALUE}_healthcare.tif \
+        --transport_path=data/tif_boundaries/${VALUE}_transport.tif \
+        --tif_output=data/tif_boundaries/${VALUE}_index_services.tif
+
+     rio cogeo create  data/tif_boundaries/${VALUE}_index_services.tif \
+        data/tif_boundaries/${VALUE}_index_services_cog.tif \
         --blocksize 512 \
         --co JPEG_QUALITY=100 \
-        --nodata -99999 \
-        -w
+        --nodata 0  -w
 
   echo "---- done  ${i} ------"
 done
 ## mv
-#cp -r $outputDir ../app/public/assets/
+cp -r $outputDir ../app/public/assets/
 
